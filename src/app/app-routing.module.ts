d25886af432f999dcec0fd97a037275305e896e0
keshavshriram <keshavshriram2002@gmail.com>
2024-06-06 13:46:24 +0530

    Worked on Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  // {
-  //   path:'/',
-  //   component : HomeComponent
-  // },
-  // {
-  //   path : 'app-about-me',
-  //   component : AboutMeComponent
-  // }
+  {
+    path:'',
+    component : HomeComponent
+  },
+  {
+    path : 'app-about-me',
+    component : AboutMeComponent
+  },
+  {
+    path : 'app-skills',
+    component : SkillsComponent
+  },
+  {
+    path : 'app-projects',
+    component : ProjectsComponent
+  },
+  {
+    path : 'app-experience',
+    component : ExperienceComponent
+  },
+  {
+    path : 'app-contact',
+    component : ContactComponent
+  }
 ];
 
 @NgModule({
