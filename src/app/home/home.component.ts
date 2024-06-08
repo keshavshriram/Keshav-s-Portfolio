19b94e232838c59099bc724ad446fd0fc4227f1f
keshavshriram <keshavshriram2002@gmail.com>
2024-06-09 01:06:40 +0530

    does work on name div and home-contact , completed with home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import { animate, query, stagger, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -13,11 +13,53 @@ import { Component } from '@angular/core';
           style({ transform: 'translate(-20px, -70px)', opacity: 0 }),
           stagger(200, [
             // Animate to the normal position and opacity 1
-            animate('500ms ease-out', style({ transform: 'translate(0, 0)', opacity: 1 }))
+            animate('250ms ease-out', style({ transform: 'translate(0, 0)', opacity: 1 }))
           ])
         ])
       ])
-    ])
+    ]),
+
+    trigger('emailAnimation', [
+      state('void', style({
+        transform: 'translateX(-75px) translateY(50px)',
+        opacity: 0,
+      })),
+      state('*', style({
+        transform: 'translateX(0) translateY(0)',
+        opacity: 1,
+      })),
+      transition('void => *', [
+        animate('1000ms ease-out')
+      ]),
+      
+    ]),
+
+    trigger('whatsappAnimation', [
+      state('void', style({
+        transform: 'translateX(75px) translateY(50px)',
+        opacity: 0,
+      })),
+      state('*', style({
+        transform: 'translateX(0) translateY(0)',
+        opacity: 1,
+      })),
+      transition('void => *', [
+        animate('1000ms ease-out')
+      ]),
+      
+    ]),
+
+    trigger('nameAnimation',[
+      state('void',style({
+        transform:'translateX(-100px)',
+        opacity:0,
+      })),
+      state('*',style({
+        transform :'translateX(0)',
+        opacity:1,
+      })),
+      transition('void=>*',[animate('400ms ease-out')]),
+    ]),
   ]
 })
 export class HomeComponent {}
