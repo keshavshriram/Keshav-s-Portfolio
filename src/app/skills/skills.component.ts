9642e9f74aa77c210f27f75022635535793ad36b
keshavshriram <keshavshriram2002@gmail.com>
2024-06-09 16:37:46 +0530

    working on skill component ,ul li designing part completed  , mouse hover effect is  remaining

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
+  animations :[
+    
+  ]
 })
 export class SkillsComponent {
 
