f3f6204d552d5a47e5afe9c50e509132208e687c
keshavshriram <keshavshriram2002@gmail.com>
2024-06-11 16:15:03 +0530

    Added animation to experience Component

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,10 +1,38 @@
+import { animate, animation, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'],
+  animations: [
+    trigger('internshipAnimation', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(-40%)',
+          opacity: 0,
+        }),
+        animate('500ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        }))
+      ])
+    ]),
+    trigger('certificateAnimation',[
+      transition(':enter',[
+        style({
+          transform:'translateX(11%)',
+          opacity:0,
+        }),
+        animate('500ms ease-out',style({
+          transform:'translateX(0)',
+          opacity:'1',
+        }))
+      ])
+    ])
+  ]
 })
 export class ExperienceComponent {
 
+  cerificates: string[] = ["Java Certificate", "Web Development", "Oracle-SQL", "Avishkar Project Competition", "Maharashtra State Certificate in IT  with 82%"];
 }
